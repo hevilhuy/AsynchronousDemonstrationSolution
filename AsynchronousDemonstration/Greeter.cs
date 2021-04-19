fa2d2f023ad1e1a64f796736c661b39664657563
Huy Tran Phuoc <huy.tranphuoc@orientsoftware.com>
2021-04-19 09:27:56 +0700

    Add project files.

---
+++ AsynchronousDemonstration/Greeter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsynchronousDemonstration
+{
+    public class Greeter
+    {
+        public async Task GreetAsync(int sec, string name)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(sec));
+            //Calling Thread.Sleep here is wrong because it will block current thread which blocks the whole program unless a new thread is created.
+            Console.WriteLine($"Greetings {name}");
+        }
+    }
+}
---
+++ AsynchronousDemonstration/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsynchronousDemonstration
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var greeter = new Greeter();
+
+            Console.WriteLine("====Awaiting Asynchronously====");
+            await AwaitingAsynchronous(greeter);
+            Console.WriteLine("====Awaiting in order====");
+            await AwaitingInOrder(greeter);
+
+            Console.ReadLine();
+        }
+
+        static async Task AwaitingAsynchronous(Greeter greeter)
+        {
+            var firstGreet = greeter.GreetAsync(3, "1");
+            var secondGreet = greeter.GreetAsync(2, "2");
+            await firstGreet;
+            await secondGreet;
+        }
+
+        static async Task AwaitingInOrder(Greeter greeter)
+        {
+            await greeter.GreetAsync(3, "1");
+            await greeter.GreetAsync(2, "2");
+        }
+    }
+}
